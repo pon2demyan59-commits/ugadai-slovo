3aab5d58ea46b8dd42af0ea95bfddc297d8ab7d6
Демьян <pon2.demyan59@gmail.com>
2026-09-26 02:21:07 +0500

    Replace initial letter reveal with one-line clue for each test word

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ugadai-slovo-v5";
+const CACHE_NAME = "ugadai-slovo-v6";
 const FILES_TO_CACHE = ["./", "./index.html", "./style.css", "./script.js", "./data/words.js", "./manifest.webmanifest", "./assets/icon.svg"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)).then(() => self.skipWaiting()));
