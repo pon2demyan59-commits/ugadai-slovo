028f7a3e0b06ded95fd69b40a2777d8f6fb681c6
Демьян <pon2.demyan59@gmail.com>
2026-09-26 02:29:41 +0500

    Prioritize unplayed words, retry misses, and retain progress

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ugadai-slovo-v7";
+const CACHE_NAME = "ugadai-slovo-v8";
 const FILES_TO_CACHE = ["./", "./index.html", "./style.css", "./script.js", "./data/words.js", "./manifest.webmanifest", "./assets/icon.svg"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)).then(() => self.skipWaiting()));
